d881a3a6fd0560360fb5c85573cfcf68446e4de3
Rin <64449865+lee-haell@users.noreply.github.com>
2024-07-22 16:13:33 +0900

    edit - compose 함수를 이용한 UI 조정(배경색 변경하기)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,3 +1,6 @@
+/**
+ * 앱을 켰을 때 가장 처음 들어 오는 곳
+ */
 package com.example.myapplication
 
 import android.os.Bundle
@@ -6,15 +9,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        /**
+         * 안드로이드에서 bundle이란 map 형태로 구현된 데이터의 묶음이다.
+         */
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
@@ -25,23 +34,32 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
+
             }
         }
     }
 }
 
+
+//@Composable
+//fun Greeting(name: String, modifier: Modifier) {
+//    Text(text = "Hello $name!")
+//}
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun Greeting(name: String, modifier: Modifier) {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Text (text = "Hello $name!")
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        Greeting("Android", androidx.compose.ui.Modifier.Companion.padding(16.dp))
     }
 }
+
+
+
