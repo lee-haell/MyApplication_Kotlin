7ee7255780193bc4f90017e97fbdba4053872c4f
Rin <64449865+lee-haell@users.noreply.github.com>
2024-07-24 18:01:15 +0900

    edit - metarial button 추가, modifier 수정

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,47 +7,54 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.layout.size
+import androidx.compose.material3.AlertDialogDefaults.shape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DividerDefaults.color
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.ui.graphics.Color
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        /**
-         * 안드로이드에서 bundle이란 map 형태로 구현된 데이터의 묶음이다.
-         */
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                MyApp(modifier = Modifier.fillMaxSize())
+                MyApp()
             }
         }
     }
+
 }
 
-/**
- * 재사용 가능한 MyApp 함수 생성
- */
+
 @Composable
 fun MyApp(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    names: List<String> = listOf("First Memo", "Second Memo")
 ) {
-    Column(modifier = modifier.padding(0.dp, 5.dp, 0.dp, 0.dp)) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
         for (name in names) {
             Greeting(name = name)
         }
@@ -57,30 +64,30 @@ fun MyApp(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    /**
-     * private 키워드는 접근 범위를 제한
-     * Greeting 함수는 동일한 파일 내에 다른 코드에서 호출될 수 있음
-     * 하지만 다른 파일에서 호출될 수 없음
-     */
     Surface(
         color = MaterialTheme.colorScheme.primary,
-        modifier = modifier.padding(10.dp, 5.dp, 10.dp, 5.dp) //감싸는 요소에 대한 padding
-    ) {
-        Column(
-            modifier = modifier
-                .fillMaxWidth() //최대 가로 크기
-        ) {
-        Column(modifier = modifier.padding(24.dp)){ //text 영역에 대한 padding
-            Text("Hello,")
-            Text("$name!")
-        }
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    )
+    {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.padding(24.dp)
+        )
+        {
+            Column(modifier = Modifier.weight(f)) {
+                Text("Hello,")
+                Text("$name!")
+            }
+                ElevatedButton(
+                    onClick = { /*TODO*/ },
+                ) {
+                    Text("Show more")
+                }
         }
     }
 }
 
-/**
- * 미리보기
- */
+
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
 fun GreetingPreview() {
@@ -88,3 +95,4 @@ fun GreetingPreview() {
         MyApp()
     }
 }
+
